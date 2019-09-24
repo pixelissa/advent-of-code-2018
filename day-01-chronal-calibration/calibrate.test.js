4e936fb7f2f5610c412a15c3c46658a9aa4d0cdf
Melissa Statham <statham_melissa@outlook.com>
2019-09-24 15:38:58 -0400

    Add tests for day 1 part 1

---
+++ day-01-chronal-calibration/calibrate.test.js
@@ -0,0 +1,38 @@
+const calibrate = require("./calibrate");
+
+// calibrate tests
+test("returns 3 when input is +1, -2, +3, +1", () => {
+    const input = 
+    `+1
+    -2
+    +3
+    +1`;
+    expect(calibrate(input)).toBe(3);
+});
+
+test("returns 3 when input is +1, +1, +1", () => {
+    const input = 
+    `+1
+    +1
+    +1`;
+    expect(calibrate(input)).toBe(3);
+});
+
+test("returns 0 when input is +1, +1, -2", () => {
+    const input = 
+    `+1
+    +1
+    -2`;
+    expect(calibrate(input)).toBe(0);
+});
+
+test("returns -6 when input is -1, -2, -3", () => {
+    const input = 
+    `-1
+    -2
+    -3`;
+    expect(calibrate(input)).toBe(-6);
+});
+
+// calibrate-two tests
+
