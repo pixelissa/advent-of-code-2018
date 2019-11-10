eede374bc25a1df036197f8d536ec1b84d8b1041
Melissa Statham <statham_melissa@outlook.com>
2019-11-10 02:32:45 -0500

    Refactor day 5 to extract common function

--- day-05-alchemical-reduction/polymer-one.js
+++ day-05-alchemical-reduction/polymer-one.js
@@ -1,42 +1,10 @@
-const input = require("fs").readFileSync("./input.txt");
+const input = require("fs").readFileSync("./input.txt").toString();
+const reactPolymer = require("./react-polymer.js");
 
 const polymerOne = (input) => {
-    let polymer = input.toString().split("");
-    let scanning = true;
-    let firstUnitPosition = 0;
-
-    while (scanning) {
-
-        let reacted = checkIfReacted(polymer[firstUnitPosition], polymer[firstUnitPosition + 1]);
-
-        if (reacted) {
-            polymer.splice(firstUnitPosition, 2);
-            //start again
-            firstUnitPosition = 0;
-        } else {
-            firstUnitPosition++;
-        }
-
-        if (!polymer[firstUnitPosition + 1]) {
-            scanning = false;
-        }
-
-        if (firstUnitPosition >= polymer.length) {
-            scanning = false;
-        }
-    }
-
-    return polymer.length;
- 
-};
-
-const checkIfReacted = (unit1, unit2) => {
-    if (unit1.toLowerCase() === unit2.toLowerCase()) {
-        if (unit1 !== unit2) {
-            return true;
-        }
-    }
-    return false;
+    let polymer = input.split("");
+  
+    return reactPolymer(polymer).length; 
 };
 
 console.log(polymerOne(input));
---
+++ day-05-alchemical-reduction/polymer-two.js
@@ -0,0 +1,29 @@
+const input = require("fs").readFileSync("./input.txt").toString();
+const reactPolymer = require("./react-polymer.js");
+
+const polymerTwo = (input) => { 
+    // let start = Date.now();
+    let uniqueUnits = getUniqueUnits(input);
+    let shortestPolymerLength = input.length;
+    
+    uniqueUnits.forEach(unit => {
+        let polymer = input.replace(new RegExp(unit, "gi"), "").split("");
+        polymer = reactPolymer(polymer);
+
+        if (polymer.length < shortestPolymerLength) {
+            shortestPolymerLength = polymer.length;
+        }
+
+    });
+      
+    // console.log((Date.now() - start)/1000);
+    return shortestPolymerLength;
+ };
+
+const getUniqueUnits = (input) => {
+    return new Set(input.toLowerCase());
+};
+
+console.log(polymerTwo(input));
+
+//module.exports = polymerTwo;
